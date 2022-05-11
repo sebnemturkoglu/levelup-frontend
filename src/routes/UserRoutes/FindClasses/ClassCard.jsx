adae2f0de2a84e8df412b353dfed52f2f65216b9
Sebnem Turkoglu <sebnem.turkoglu@ug.bilkent.edu.tr>
2022-05-11 10:26:48 +0300

    card component for listing classes

--- src/routes/UserRoutes/FindClasses/ClassCard.jsx
+++ src/routes/UserRoutes/FindClasses/ClassCard.jsx
@@ -1,46 +1,56 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CardActionArea from "@mui/material/CardActionArea";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import CardImage from "../../../assets/CourseCardImage.png";
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import { ListItemButton, Box } from '@mui/material';
 
 export default function ClassCard() {
   const theme = useTheme();
 
   return (
-    <Card sx={{ display: "flex", height: "90px" }}>
-      <CardMedia
-        component="img"
-        sx={{ width: 70, margin:'4px' }}
-        image={CardImage}
-      />
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="body2">
-            Writing for DELF1
-          </Typography>
-          <Typography
-            variant="caption"
-            color="text.secondary"
-            component="div"
-          >
-            Instructor: Sırma Kuzukulak
-          </Typography>
-          <Typography
-            variant="caption"
-            color="text.secondary"
-            component="div"
-          >
-            Duration: 8 weeks
-          </Typography>
-        </CardContent>
-      </Box>
-    </Card>
+    <ListItem alignItems="flex-start" sx={{padding:0, bgcolor: 'background.paper', marginBottom:'12px', borderRadius:'8px'}}>
+      <ListItemButton sx={{padding:'8px 12px'}}>
+        <ListItemAvatar sx={{marginRight:'8px'}} >
+          <img src={CardImage}  />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Writing for DELF B1"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="caption"
+                color="text.primary"
+              >
+                Instructor: Sırma Kuzukulak
+              </Typography>
+              <Box>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="caption"
+                color="text.primary"
+              >
+                Duration: 6 weeks
+              </Typography>
+              <Typography
+                sx={{ display: 'inline', marginLeft:'64px', right:0 }}
+                component="span"
+                variant="caption"
+                color="text.primary"
+              >
+                Intermediate
+              </Typography>
+              
+              </Box>
+            </React.Fragment>
+          }
+        />
+         </ListItemButton>
+      </ListItem>
   );
 }
--- src/routes/UserRoutes/FindClasses/ClassList.jsx
+++ src/routes/UserRoutes/FindClasses/ClassList.jsx
@@ -1,14 +1,46 @@
-import * as React from 'react';
-import  {useDispatch, useSelector}  from 'react-redux';
-import ClassCard from './ClassCard';
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ClassCard from "./ClassCard";
+import { ListSubheader, List, Box, Typography, Button } from '@mui/material'
 
 const ClassList = (props) => {
+  //   const [open, setOpen] = React.useState(true);
+  //   const dispatch = useDispatch();
 
-//   const [open, setOpen] = React.useState(true);
-//   const dispatch = useDispatch();
+  const facilityData = [1, 2, 3, 4];
 
-    return( <ClassCard /> );
-}
+  return (
+    <Box
+      sx={{
+        minWidth: "350px",
+        maxWidth: "500px",
+        bgcolor: "#F0F5ED",
+        p: 5,
+        borderRadius: "22px 0px 0px 42px",
+      }}
+    >
+      <List
+        sx={{ width: "100%" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader
+            component="div"
+            id="nested-list-subheader"
+            style={{ backgroundColor: "transparent", color: "#fff" }}
+          >
+            <Typography variant="h5" color={'secondary'}>Find Classes</Typography>
+          </ListSubheader>
+        }
+      >
+        {facilityData.map((center) => {
+          return (
+            <ClassCard />
+          );
+        })}
+      </List>
+    </Box>
+  );
+};
 
 export default ClassList;
-
